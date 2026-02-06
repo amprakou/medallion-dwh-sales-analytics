eaea50fac9b268866ff40908c7defcbd80d71ada
amprakou <amprakou@outlook.com>
2026-02-06 15:40:08 +0200

    Update fact_sales.sql

--- sql_scripts/05_gold_layer/fact_sales.sql
+++ sql_scripts/05_gold_layer/fact_sales.sql
@@ -4,7 +4,7 @@
 -- Purpose: Sales transactions fact table
 -- One row per product per sales order
 -- Sources: silver.crm_sales_details, gold.dim_products, gold.dim_customers
--- Foreign Keys: customer_key, product_key, order_date_key (to be joined with dim_date)
+-- Foreign Keys: customer_key, product_key
 -- =============================================
 
 USE DataWarehouse;
@@ -20,7 +20,6 @@ SELECT
     pr.product_key AS product_key,
     cu.customer_key AS customer_key,
     sd.sls_order_dt AS order_date,
-    CAST(FORMAT(sd.sls_order_dt, 'yyyyMMdd') AS INT) AS order_date_key,
     sd.sls_ship_dt AS shipping_date,
     sd.sls_due_dt AS due_date,
     sd.sls_sales AS sales_amount,
