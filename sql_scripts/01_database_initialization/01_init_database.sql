f4f1fed934f183f9a59c0190b95f3b6eaa86345b
amprakou <amprakou@outlook.com>
2026-02-06 15:49:08 +0200

    v_bug

--- sql_scripts/01_database_initialization/01_init_database.sql
+++ sql_scripts/01_database_initialization/01_init_database.sql
@@ -23,14 +23,14 @@ GO
 -- Step 2: Create Schemas (Medallion Architecture)
 -- =============================================
 
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
-    CREATE SCHEMA bronze;
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'bronze')
+    EXEC('CREATE SCHEMA bronze');
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
-    CREATE SCHEMA silver;
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'silver')
+    EXEC('CREATE SCHEMA silver');
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
-    CREATE SCHEMA gold;
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'gold')
+    EXEC('CREATE SCHEMA gold');
 GO
